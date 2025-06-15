ef5ad8a877711195b5aceaf1808ddf239639e8a8
Narek Pogosian <narekpogosian2@gmail.com>
2025-06-15 19:25:46 +0200

    fix: better design for product card on mobile

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 ## TODO
 
+- [x] Better design for descriptive cards on mobile
 - [x] Tags and category attributes filter
 - [x] Descriptive grid toggle
 - [x] Details page
 - [x] Cart
 - [x] Fake Order
 
-- [ ] Better design for descriptive cards on mobile
 - [ ] Add tsvector column in neon, FTS search and similar products
--- src/components/products/product-grid.tsx
+++ src/components/products/product-grid.tsx
@@ -13,7 +13,7 @@ export default function ProductGrid({
   return (
     <ul
       className={cn("grid w-full gap-10", {
-        "grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-3 lg:gap-10 xl:grid-cols-4":
+        "grid-cols-2 gap-4 sm:grid-cols-3 sm:gap-6 md:grid-cols-4 lg:grid-cols-3 lg:gap-10 xl:grid-cols-4":
           !showDescriptiveGrid,
       })}
     >
